dd4a63a7a0f3d4f55f4ea70467e70db3d7b1c252
Tan Pham <tanpham.hust@gmail.com>
2017-11-11 10:25:08 +0700

    UPDATE MENU + FIREBASE

---
+++ Contra2/Assets/_Scripts/ItemPrice.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ItemPrice {
+
+    public static int NORMAL_ARMO = 5;
+    public static int LAZE_ARMO = 10;
+    public static int FIRE_ARMO = 15;
+    public static int RADIATE_ARMO = 20;
+
+    public static int LIFE = 30;
+}
---
+++ Contra2/Assets/_Scripts/RefDefinition.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class RefDefinition {
+    public static string UID = "uid";
+}
